2cddea137de820d6230b2df5275c2098c5b166dd
AmiMendapara <amipatel42429@gmail.com>
2024-07-28 17:30:37 +0530

    new commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 // src/App.js
 
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import LandingPage from './LandingPage';
 import CommunityPage from './CommunityPage';
 import './App.css';
